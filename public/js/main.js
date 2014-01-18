2a742e75a21f077d57ef571dd17bc645874194c4
Vishal Mathur <vishmathur5@gmail.com>
2014-01-18 15:52:02 -0500

    trying out database flow for getting users

--- public/js/main.js
+++ public/js/main.js
@@ -32,13 +32,12 @@ $(function() {
 
 });
     
-    //this is code for getting data from the database
-    //
-    // $.get("http://localhost:3000/data",function(data,status){
-    //   console.log("")
-    //   /*alert("Data: " + data + "\nStatus: " + status);*/
-    //   console.log(data);
-    // });
+function getUserData(){
+  $.get("http://localhost:3000/data/get/getUser",function(data,status){
+    console.log(data)
+  });
+}
+
 
 
 function postUserData() {
