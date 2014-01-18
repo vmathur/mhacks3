653cef770d9dd44a38853dbbcc8531ca9d67d57b
whatarthurcodes <arthur.yan3@gmail.com>
2014-01-18 03:41:18 -0500

    yuruuurur moma:

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
  		alert("You've clicked the studytab")
   });
 
+  $("#Homebtn").click(function(){
+ 	var f = document.createElement("form");
+	f.setAttribute('method',"post");
+	f.setAttribute('action',"submit.php");
+  });
+
+
+
+
+
+
 });
+
+
+
+var c = document.createElement("input");
+c.type = "checkbox";
+c.id = "checkbox1";
+c.name = "check1";
+
+
