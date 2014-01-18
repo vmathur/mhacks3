0e72c48db88e88b6e0ff41fcfd6933821d7d295a
Vishal Mathur <vishmathur5@gmail.com>
2014-01-18 09:30:24 -0500

    lets get some mongo up in dissssss

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var   http = require('http');
 var   path = require('path');
 var   handle = require('./routes/handle');
 
+//mongo stuff
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/mydb');
+
+
 var app = express();
+
+
 
 app.configure('development', function(){
   app.use(express.errorHandler());
@@ -18,7 +26,7 @@ app.set('title', 'Awesome Webapp');
 
 app.get('/', routes.index);
 app.get('/update', handle.update);
-
+//app.get('/userlist', routes.userlist(db));
 
 app.listen(3000);
 console.log('Listening on port 3000');
