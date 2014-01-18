12cd85856522617edab0f542305c35e6f97d458d
whatarthurcodes <arthur.yan3@gmail.com>
2014-01-18 07:15:25 -0500

    Merging Map back in with Noorain's working stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
+
 $( document ).ready(function() {
-  var map;
+var map;
 function initialize() {
   var mapOptions = {
     zoom: 14,
@@ -9,9 +10,9 @@ function initialize() {
       mapOptions);
 }
 
-
 google.maps.event.addDomListener(window, 'load', initialize);
 });
+
 
 
 
