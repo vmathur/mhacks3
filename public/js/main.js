0a5fcd92bfdfed296a30042bd282aca9aa5c8226
Vishal Mathur <vishmathur5@gmail.com>
2014-01-18 18:55:51 -0500

    trying to get dynamic load wokring

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,17 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+  getUserStudying('Science', function(data){
+      for (var i = 0; i<data.length;i++){
+          var longu = data[i].location.long - 83.0458;
+          var lati = data[i].location.lati + 42.3384;
+          var studentname = data[i].name;
+
+          createMarker(lati,longu, studentname);
+          //console.log(data[i].username);
+      }
+  });
+
   getLocation();
   createMarker(42.3354,-83.0428, "Bob is here");
  }
@@ -21,10 +32,6 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 $(function() {
   $("#home-image").click(function(){
-    getUserStudying('Science', function(data){
-      //do something meaningul with data
-      console.log(data);
-    });
   });
 });
  
