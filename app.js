5cdf2df3fdac1d80816dffbaf75579ad3b0021eb
Vishal Mathur <vishmathur5@gmail.com>
2014-04-18 17:17:08 -0400

    lets hope this db stuff works

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var dataGet  = require('./routes/dataGet');
 var dataPost = require('./routes/dataPost');
 
 var DB_LOCATION = process.env.MONGOHQ_URL||'localhost:27017';
+
+
 console.log('using db: '+DB_LOCATION);
 var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient
 var monk = require('monk');
-var db = monk(DB_LOCATION+'/buddyup');
+// var db = monk(DB_LOCATION+'/buddyup');
 
 var MongoClient = mongo.MongoClient;
 
@@ -31,11 +34,11 @@ app.get('/', routes.index);
 app.get('/map',routes.map);
 app.get('/update', handle.update);
 
-//MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+MongoClient.connect(DB_LOCATION, function(err, db) {
 	app.get('/data/get/getUserWith/:stuff', dataGet.getUserWith(db));
 	app.get('/data/get/getUser', dataGet.getUser(db));
 	app.post('/data/post/user', dataPost.addUser(db));
-//});
+});
 
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000');
